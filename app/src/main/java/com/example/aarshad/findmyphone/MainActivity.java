aad115a163172f8a26639ed12846e5289e8c9f6a
Arslan <aac.1989@hotmail.com>
2017-07-05 21:25:23 +0900

    Tracking Whom & Tracked By Working

--- app/src/main/java/com/example/aarshad/findmyphone/MainActivity.java
+++ app/src/main/java/com/example/aarshad/findmyphone/MainActivity.java
@@ -5,23 +5,116 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.location.LocationManager;
 import android.os.Build;
+import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    TrackerAdapter trackerAdapter ;
+    ArrayList<AdapterItems> listTrackers = new ArrayList<AdapterItems>();
+    DatabaseReference mDatabaseReference ;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refreshDb();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        checkLocationPermission();
+
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
         GlobalInfo globalInfo = new GlobalInfo(this);
         globalInfo.loadData();
+        checkLocationPermission();
 
+        ListView listView = (ListView) findViewById(R.id.listView);
+        trackerAdapter = new TrackerAdapter(this, R.layout.single_row_contact, listTrackers);
+        listView.setAdapter(trackerAdapter);
+
+
+    }
+    private void refreshDb() {
+        listTrackers.clear();
+
+
+        mDatabaseReference.child("Users").child(GlobalInfo.phoneNumber).
+                child("Finders").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Toast.makeText(getApplicationContext(),"Listned to Finders",Toast.LENGTH_SHORT).show();
+
+                Map<String, Object> td = (HashMap<String, Object>) dataSnapshot.getValue();
+
+                listTrackers.clear();
+                if (td == null)  //no one allow you to find him
+                {
+                    listTrackers.add(new AdapterItems("NoTicket", "no_desc"));
+                    trackerAdapter.notifyDataSetChanged();
+                    return;
+                }
+                // List<Object> values = td.values();
+
+                // get all contact to list
+                ArrayList<AdapterItems> listContact = new ArrayList<AdapterItems>();
+                Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+                while (cursor.moveToNext()) {
+                    String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+
+                    String phoneNumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    listContact.add(new AdapterItems(  name,GlobalInfo.formatPhoneNumber(phoneNumber)
+                    ));
+
+                }
+
+// if the name is save chane his text
+                // case who find me
+                String tinfo;
+                for (  String Numbers : td.keySet()) {
+                    for (AdapterItems cs : listContact) {
+
+                        //IsFound = SettingSaved.WhoIFindIN.get(cs.Detals);  // for case who i could find list
+                        if (cs.phoneNumber.length() > 0)
+                            if (Numbers.contains(cs.phoneNumber)) {
+                                listTrackers.add(new AdapterItems(cs.userName, cs.phoneNumber));
+                                break;
+                            }
+                    }
+                }
+                trackerAdapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                // Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        trackerAdapter.notifyDataSetChanged();
     }
 
     public void startService(){
@@ -38,6 +131,32 @@ public class MainActivity extends AppCompatActivity {
             startService(i);
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.addtracker:
+                Intent i = new Intent(this, MyTrackers.class);
+                startActivity(i);
+                return true;
+            case R.id.help:
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     void checkLocationPermission(){
         if ( Build.VERSION.SDK_INT >= 23){
